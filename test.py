542e641833e435887d5477703a457900933aaac7
3l-Sabio <xavier.j.montoya@ucdenver.edu>
2026-01-29 20:42:37 -0700

    continued development

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from OpenSTRAN.Model import Model
+import time
 
 # instantiate an empty model
 simpleBeam = Model()
@@ -14,13 +15,19 @@ N1.restraint = [1, 1, 1, 0, 0, 0]  # [Ux, Uy, Uz, φx, φy, φz] -> pinned node
 N2.restraint = [1, 1, 1, 0, 0, 0]  # [Ux, Uy, Uz, φx, φy, φz] -> pinned node
 
 # define a member between nodes N1 and N2.
-M1 = simpleBeam.members.addMember(N1, N2)  # (i node, j node)
+M1 = simpleBeam.members.addMember(N1, N2, mesh=50)  # (i node, j node)
 
 # add a load of -1 kips in the global Y direction along M1's span.
 M1.addTrapLoad(-1, -1, 'Y', 0, 100)
 
+# print(simpleBeam.nodes.properties())
+
+start = time.time()
 # solve the model.
 simpleBeam.solve()
+stop = time.time()
+
+print(f"time to run: {stop-start} seconds")
 
 # print the nodal reactions to the terminal.
-simpleBeam.reactions()
+# simpleBeam.reactions()
